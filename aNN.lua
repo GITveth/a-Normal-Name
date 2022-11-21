0b336ab8b4746a045e086968b2664e1ec9135534
GITveth <118706955+GITveth@users.noreply.github.com>
2022-11-20 22:56:12 -0600

    Update aNN.lua

--- aNN.lua
+++ aNN.lua
@@ -1,3 +1,14 @@
-for i = 1, 5 do
-   print(i)
+function sayHello()
+   print("Hello, world")
 end
+sayHello()
+
+function addTwoNumbers(a, b)
+   print("The sum is:", a + b)
+end
+addTwoNumbers(3, 5)
+
+function calculateSquare(n)
+   return n * n
+end
+local result = calculateSquare(3)
