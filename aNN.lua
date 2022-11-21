2a457ce90af80ef73d48ee7e7c0769a1d38cf6d8
GITveth <118706955+GITveth@users.noreply.github.com>
2022-11-20 22:46:03 -0600

    Update aNN.lua

--- aNN.lua
+++ aNN.lua
@@ -1,1 +1,10 @@
-print('Hi Mom')
+local list = {1, 2, 3}
+local firstNum = list[1]
+list[2] = 4
+print("There are " .. #list .. " numbers")
+
+local total = 0
+for i = 1, #list do
+   total = total + list[i]
+end
+print("The total is " .. total)
