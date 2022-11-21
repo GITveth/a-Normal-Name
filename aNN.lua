d8c16221fe5cdaf35e16522ac213cbbe144c1bbc
GITveth <118706955+GITveth@users.noreply.github.com>
2022-11-20 23:25:04 -0600

    Update aNN.lua

--- aNN.lua
+++ aNN.lua
@@ -1,5 +1,6 @@
 local a = math.random(1,2)
 local b = math.random(1,2)
 
-if a < b print('A is greater than B!')
+if a <= b print('A is greater than B!')
   else print('A is Less than or equal to B!')
+end
