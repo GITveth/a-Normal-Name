ff23b80fa2e12009c34bc5c020db476efa65ec72
GITveth <118706955+GITveth@users.noreply.github.com>
2022-11-20 22:48:58 -0600

    Update aNN.lua

--- aNN.lua
+++ aNN.lua
@@ -1,10 +1,3 @@
-local list = {1, 2, 3}
-local firstNum = list[1]
-list[2] = 4
-print("There are " .. #list .. " numbers")
-
-local total = 0
-for i = 1, #list do
-   total = total + list[i]
+for i = 1, 5 do
+   print(i)
 end
-print("The total is " .. total)
