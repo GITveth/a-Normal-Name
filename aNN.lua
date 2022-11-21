6f31e10ec273a2b04e8e673916e3882741e16308
GITveth <118706955+GITveth@users.noreply.github.com>
2022-11-21 00:17:39 -0600

    Update aNN.lua

--- aNN.lua
+++ aNN.lua
@@ -1,8 +1,23 @@
-local num = math.random(1,100)
-
-function roundToNearestEven(num)
-  if num % 2 ~= 0 then
-    num = num + 1
-  end
-  return num
-end
+function mostUsedLetter(A quick brown fox jumps over the lazy dog.) {
+  var letterCounts = {};
+  var mostUsed = "";
+  var mostCount = 0;
+  
+  for (var i = 0; i < sentence.length; i++) {
+    var letter = sentence[i];
+    
+    if (letterCounts[letter]) {
+      letterCounts[letter]++;
+    }
+    else {
+      letterCounts[letter] = 1;
+    }
+    
+    if (letterCounts[letter] > mostCount) {
+      mostUsed = letter;
+      mostCount = letterCounts[letter];
+    }
+  }
+  
+  return mostUsed;
+}
