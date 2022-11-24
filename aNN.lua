c4517f90fe04c123b1df1ba0b060934912a4b48a
GITveth <118706955+GITveth@users.noreply.github.com>
2022-11-24 08:59:21 -0600

    Update aNN.lua

--- aNN.lua
+++ aNN.lua
@@ -1,6 +1,6 @@
 _G.autotap = true
 while _G.autoTap == true do
-    local args = {[1] - 1)
+    local args = {[1] = 1)
     game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.ClickService.Click:FireServer(1)
     wait()
 end
