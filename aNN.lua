f9c5db825d324f39a9b8ed1674d4d4b5381cfe3a
GITveth <118706955+GITveth@users.noreply.github.com>
2022-11-23 20:27:56 -0600

    Update aNN.lua

--- aNN.lua
+++ aNN.lua
@@ -1,24 +1,9 @@
-local sentence = "The quick brown fox jumps over the lazy dog."
-
-local mostUsed = {}
-
-for i = 1, #sentence do
-    local char = sentence:sub(i, i)
-    if mostUsed[char] then
-        mostUsed[char] = mostUsed[char] + 1
-    else
-        mostUsed[char] = 1
-    end
+_G.autotap = true
+while _G.autoTap == true do
+    local args = {[1] - 1)
+    game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.Clickservice.Click:FireServer(unpack(args))
+    wait()
 end
 
-local maxCount = 0
-local maxChar = ""
 
-for char, count in pairs(mostUsed) do
-    if count > maxCount then
-        maxCount = count
-        maxChar = char
-    end
-end
 
-print(maxChar, maxCount)
